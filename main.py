02206d19fa3e841caa776e620f25c95f41c1ce81
simpletondl <simpletondl@yandex.ru>
2020-08-16 11:53:24 +0300

    test final workflow with docker

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from pygraphblas import *
 from tqdm import tqdm
+from cfpq_data_devtools.filters import get_file_len
 
 A = Matrix.sparse(BOOL, 5, 5)
 B = Matrix.sparse(BOOL, 5, 5)
 A[1, 2] = True
 B[2, 3] = True
 
+print(list(A), list(B))
 assert list(A @ B) == [(1, 3, True)]
 
 for i in tqdm(range(100), desc="Testing tqdm"):
     pass
+
+print(get_file_len(__file__))
