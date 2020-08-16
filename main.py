b9abedd520b3d807fe4c6acdacb7126ab18da1fe
simpletondl <simpletondl@yandex.ru>
2020-08-16 11:34:11 +0300

    test improved workflow with docker

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello from CI!')
+from pygraphblas import *
+from tqdm import tqdm
+
+A = Matrix.sparse(BOOL, 5, 5)
+B = Matrix.sparse(BOOL, 5, 5)
+A[1, 2] = True
+B[2, 3] = True
+
+assert list(A @ B) == [(1, 3, True)]
+
+for i in tqdm(range(100), desc="Testing tqdm"):
+    pass
